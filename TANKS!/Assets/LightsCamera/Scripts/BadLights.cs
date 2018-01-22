986e5a36ae6c406759f31274c4f063fd17e15162
charlesDouc <charles.douc@gmail.com>
2018-01-22 15:39:22 -0500

    Class Monday Jan 22

---
+++ TANKS!/Assets/LightsCamera/Scripts/BadLights.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadLights : MonoBehaviour {
+	
+	// Public variables
+	public GameObject thePlayer;		// GameObject 
+
+
+	// ----------------------------------------
+	// Use this for initialization
+	// ----------------------------------------
+	void Start () {
+		
+	}
+
+
+	// ----------------------------------------
+	// Update is called once per frame
+	// ----------------------------------------
+	void Update () {
+		
+		
+	}
+
+	// ----------------------------------------
+	// Methods
+	// ----------------------------------------
+	void OnTriggerEnter() {
+		// Get the antennaStatus of the tank light
+		lightControl TankLightScript = thePlayer.GetComponent <lightControl> ();
+		TankLightScript.antennaStatus = false;
+		//Debug.Log ("Enter");
+	}
+
+	void OnTriggerExit() {
+		// Get the antennaStatus of the tank light
+		lightControl TankLightScript = thePlayer.GetComponent <lightControl> ();
+		TankLightScript.antennaStatus = true;
+		//Debug.Log ("Enter");
+	}
+
+}
