fb88eaa5960670436d0dbf388656b82b9d99d9d8
charlesDouc <charles.douc@gmail.com>
2018-01-29 09:28:40 -0500

    Added cameras effect

---
+++ TANKS!/Assets/cameraTriggers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraTriggers : MonoBehaviour {
+	
+	// public variables
+	public int cameraIndex;
+	public GameObject cameraControls;
+
+	// private variables
+	private bool goBack;
+
+
+
+	// ---------------------------------------
+	// Use this for initialization
+	// ---------------------------------------
+	void Start () {
+		goBack = false;
+	}
+
+	// ---------------------------------------
+	// Update is called once per frame
+	// ---------------------------------------
+	void Update () {
+		
+	}
+
+	// ---------------------------------------
+	// Methods
+	// ---------------------------------------
+	void OnTriggerEnter () {
+		cameras_control mainCameraScript = cameraControls.GetComponent <cameras_control> ();
+		if (goBack) {
+			mainCameraScript.changeCamera (cameraIndex - 1);
+			goBack = false;
+			Debug.Log ("Camera " + cameraIndex);
+		} else {
+			mainCameraScript.changeCamera (cameraIndex);
+			goBack = true;
+		}
+	}
+		
+
+
+}
