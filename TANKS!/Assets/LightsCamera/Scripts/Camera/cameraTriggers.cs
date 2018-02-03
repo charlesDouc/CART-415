390bf20b0c3660d53e4ae347766ddb64096c116b
charlesDouc <charles.douc@gmail.com>
2018-02-03 15:34:26 -0500

    The game begins to look like something

--- TANKS!/Assets/LightsCamera/Scripts/Camera/cameraTriggers.cs
+++ TANKS!/Assets/LightsCamera/Scripts/Camera/cameraTriggers.cs
@@ -7,6 +7,8 @@ public class cameraTriggers : MonoBehaviour {
 	// public variables
 	public int cameraIndex;
 	public GameObject cameraControls;
+	public bool toBeDestroyed;
+	public GameObject nextTrigger;
 
 	// private variables
 	private bool goBack;
@@ -21,24 +23,31 @@ public class cameraTriggers : MonoBehaviour {
 	}
 
 	// ---------------------------------------
-	// Update is called once per frame
-	// ---------------------------------------
-	void Update () {
-		
-	}
-
-	// ---------------------------------------
 	// Methods
 	// ---------------------------------------
 	void OnTriggerEnter () {
+		// Get the cameras control script
 		cameras_control mainCameraScript = cameraControls.GetComponent <cameras_control> ();
+		// If the player is going back
 		if (goBack) {
+			// Change the camera with the index - 1
 			mainCameraScript.changeCamera (cameraIndex - 1);
 			goBack = false;
-			Debug.Log ("Camera " + cameraIndex);
+			//Debug.Log ("Camera " + cameraIndex)
+
+		// If the player is going foward
 		} else {
+			// Change the camera with the index	
 			mainCameraScript.changeCamera (cameraIndex);
 			goBack = true;
+		}
+
+		// If the triggers need to be destroyed (changing zone)
+		if (toBeDestroyed) {
+			// Activate next gameobject trigger
+			nextTrigger.SetActive (true);
+			// Destroy this object
+			Destroy (gameObject);
 		}
 	}
 		
---
+++ TANKS!/Assets/LightsCamera/Scripts/Camera/changeZoneTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeZoneTrigger: MonoBehaviour {
+	
+	// public variables
+	public int cameraIndex;
+	public GameObject cameraControls;
+	public bool toBeDestroyed;
+	public GameObject nextTrigger;
+
+	// private variables
+	private bool goBack;
+
+
+
+	// ---------------------------------------
+	// Use this for initialization
+	// ---------------------------------------
+	void Start () {
+		goBack = false;
+	}
+
+	// ---------------------------------------
+	// Methods
+	// ---------------------------------------
+	void OnTriggerEnter () {
+		// Get the cameras control script
+		cameras_control mainCameraScript = cameraControls.GetComponent <cameras_control> ();
+		// If the player is going back
+		if (goBack) {
+			// Change the camera with the index - 1
+			mainCameraScript.changeCamera (cameraIndex - 1);
+			goBack = false;
+			//Debug.Log ("Camera " + cameraIndex)
+
+		// If the player is going foward
+		} else {
+			// Change the camera with the index	
+			mainCameraScript.changeCamera (cameraIndex);
+			goBack = true;
+		}
+
+		// If the triggers need to be destroyed (changing zone)
+		if (toBeDestroyed) {
+			// Activate next gameobject trigger
+			nextTrigger.SetActive (true);
+			// Destroy this object
+			Destroy (gameObject);
+		}
+	}
+		
+
+
+}
