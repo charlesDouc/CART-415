7521c33f89aab11b3a98aa82ac4c195735453d53
charlesDouc <charles.douc@gmail.com>
2018-04-15 17:52:09 -0400

    Menu is almost done.

---
+++ TANKS!/Assets/TorchLightMainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLightMainMenu : MonoBehaviour {
+
+	// public variables
+	public GameObject theTotch;
+
+	// private variables
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		StartCoroutine("blink");
+	}
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+	IEnumerator blink() {
+		while (true) {
+			theTotch.SetActive (true);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (false);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (true);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (false);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (true);
+			yield return new WaitForSeconds(4f);
+			theTotch.SetActive (false);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (true);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (false);
+			yield return new WaitForSeconds(0.1f);
+			theTotch.SetActive (true);
+			yield return new WaitForSeconds(4f);
+		}
+	}
+
+}
---
+++ TANKS!/Assets/cubeMainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeMainMenu : MonoBehaviour {
+
+	// public variables
+	public bool up = true;
+
+	// private variables
+	private float m_speed = 0.01f;
+	private bool goUp = false;
+	private bool goDown = false;
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		// Check the direction of the object
+		if (!up) {
+			m_speed = -m_speed;
+		}
+
+		// Start movement coroutine
+		StartCoroutine ("movement");
+		
+	}
+
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		Vector3 currentPos = transform.position;
+
+		if (goUp) {
+			currentPos.y += m_speed;
+		}
+
+		if (goDown) {
+			currentPos.y -=m_speed;
+		}
+
+		// Update value
+		transform.position = currentPos;
+	}
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+	IEnumerator movement () {
+		while (true) {
+			goUp = true;
+			goDown = false;
+
+			yield return new WaitForSeconds(4);
+
+			goUp = false;
+			goDown = true;
+
+			yield return new WaitForSeconds(4);
+		}		
+	}
+
+}
